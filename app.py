d28b30677f88cb2e0dce4995936d894caecc5ab1
kasalehi <ksalehi56@yahoo.com>
2025-01-11 00:43:35 +1300

    keyvan177

--- app.py
+++ app.py
@@ -5,22 +5,23 @@ import datetime
 
 # lets create log function to record all running 
 def log_key(msag):
-    with open('log.txt','a') as f:
-        f.write(f"the masg succesfuily write in {datetime.datetime.now()}-{msag}")
+     with open('log.txt','a') as f:
+         f.write(f"the masg succesfuily write in {datetime.datetime.now()}-{msag}")
         
         
 # lets rad data form url link
-response=requests.get('https://jsonplaceholder.typicode.com/posts')
-data=response.json()
+
 
 
 try :
-    # lets create data frame from json data
-    df=pd.DataFrame(data)
-    # lets print data frame
-    df.to_csv('key.csv',index=False)
-    log_key(f'data succesfuly read from url at {datetime.datetime.now()}')
+     response=requests.get('https://jsonplaceholder.typicode.com/posts')
+     data=response.json()
+     # lets create data frame from json data
+     df=pd.DataFrame(data)
+     # lets print data frame
+     df.to_csv('key.csv',index=False)
+     log_key(f'data succesfuly read from url at {datetime.datetime.now()}')
 except Exception as e:
-    log_key(f"error {e}")
-    print(f"error {e}")
+     log_key(f"error {e}")
+     print(f"error {e}")
     
